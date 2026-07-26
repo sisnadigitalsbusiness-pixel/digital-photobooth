96d92c5e910b884e8643cfa59982d6e5fcbcfed9
sisna digital <sisnadigitals.business@gmail.com>
2026-07-26 11:09:06 +0800

    Debug upload

--- upload.js
+++ upload.js
@@ -2,36 +2,34 @@ const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzDC9D2ICPdXyvwyUorC
 
 async function uploadToDrive(imageData) {
 
+    const filename = "Photostrip_" + Date.now() + ".png";
+
     try {
 
-        const filename = "Photostrip_" + Date.now() + ".png";
+        console.log("Uploading to Drive...");
 
         const response = await fetch(SCRIPT_URL, {
-
             method: "POST",
-
             headers: {
                 "Content-Type": "text/plain;charset=UTF-8"
             },
-
             body: JSON.stringify({
-
                 filename: filename,
                 image: imageData
-
             })
-
         });
 
         const text = await response.text();
 
-        console.log("Drive Upload:", text);
+        console.log("Server Response:", text);
 
-    }
+        alert("UPLOAD RESPONSE:\n\n" + text);
 
-    catch (err) {
+    } catch (err) {
 
         console.error("Upload Error:", err);
+
+        alert("UPLOAD FAILED:\n\n" + err);
 
     }
 
